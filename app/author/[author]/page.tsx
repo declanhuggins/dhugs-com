ed9e587142dfe47ff4141dcb1536d68535cbe11b
Declan <199662174+declanhuggins@users.noreply.github.com>
2025-02-20 00:59:24 -0500

    Fix for cloudflare

--- app/author/[author]/page.tsx
+++ app/author/[author]/page.tsx
@@ -1,3 +1,4 @@
+export const runtime = 'edge';
 import React, { JSX } from 'react';
 import { getAllPosts } from '../../../lib/posts';
 import PostPreview from '../../../app/components/PostPreview';
