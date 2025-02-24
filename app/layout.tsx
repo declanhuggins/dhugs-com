be4d72d2bcbbb7912a8cacf6c51d80cec27709eb
Declan <199662174+declanhuggins@users.noreply.github.com>
2025-02-24 15:29:27 -0500

    Fixed dark/light mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,7 @@ import { ReactNode } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { ThemeProvider } from 'next-themes';
-import Container from './components/Container';
-import PageLoader from './components/PageLoader'; // new import
+import Body from './components/Body';
 
 export const metadata = {
   title: 'Declan Huggins | Photographer | Computer Scientist',
@@ -14,12 +13,11 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning className="bg-[var(--background)] text-[var(--foreground)] font-mono">
-        <ThemeProvider attribute="data-theme" defaultTheme="dark" enableSystem={true}>
+        <ThemeProvider attribute="data-theme" defaultTheme="system" enableSystem={true}>
           <Header />
-          {/* Wrap Container with PageLoader for all pages */}
-          <PageLoader>
-            <Container>{children}</Container>
-          </PageLoader>
+          <Body>
+            {children}
+          </Body>
           <Footer />
         </ThemeProvider>
       </body>
