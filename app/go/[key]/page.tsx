1b7f23d056736eae5fc5d2efc8810b5fc81470c7
Declan <199662174+declanhuggins@users.noreply.github.com>
2025-02-26 18:51:47 -0500

    Update README.md with project details, add SECURITY.md, and implement key redirect functionality

---
+++ app/go/[key]/page.tsx
@@ -0,0 +1,26 @@
+import { redirect, notFound } from 'next/navigation'
+import { JSX } from 'react';
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+
+interface PageProps {
+  params: Promise<{ 
+    key: string
+  }>;
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+}
+
+export default async function KeyRedirect({ params }: PageProps): Promise<JSX.Element> {
+  const { key } = await params
+  const filePath = path.join(process.cwd(), 'links/links.md')
+  const fileContent = fs.readFileSync(filePath, 'utf8')
+  const { data } = matter(fileContent)
+  
+  const targetURL = data[key]
+  if (targetURL) {
+    redirect(targetURL)
+  } else {
+    notFound()
+  }
+}
