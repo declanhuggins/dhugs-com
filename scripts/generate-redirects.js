9a365268dea450a627985eb265297ef946152a6a
Declan <199662174+declanhuggins@users.noreply.github.com>
2025-02-26 19:42:30 -0500

    Refactor redirects: remove quotes from URLs, update file paths, and migrate redirects to public directory

--- scripts/generate-redirects.js
+++ scripts/generate-redirects.js
@@ -3,15 +3,12 @@ const path = require('path');
 const matter = require('gray-matter');
 
 const linksFilePath = path.join(__dirname, '../links/links.md');
-const redirectsFilePath = path.join(__dirname, '../_redirects');
+const redirectsFilePath = path.join(__dirname, '../public/_redirects');
 
 const fileContent = fs.readFileSync(linksFilePath, 'utf8');
 const { data } = matter(fileContent);
 
-const redirects = Object.entries(data).map(([key, url]) => {
-  const cleanedUrl = url.replace(/^“|”$/g, ''); // Remove surrounding quotes
-  return `/${key}/ ${cleanedUrl} 302`;
-}).join('\n');
+const redirects = Object.entries(data).map(([key, url]) => `/${key}/ ${url} 302`).join('\n');
 
 fs.writeFileSync(redirectsFilePath, redirects);
 
