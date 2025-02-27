314d54d8b53b964c6b9e8d340e7faf03e6ed9a07
Declan <199662174+declanhuggins@users.noreply.github.com>
2025-02-26 19:20:36 -0500

    Update README.md with build instructions, add generate-redirects script, and remove deprecated app/go/[key]/page.tsx

---
+++ scripts/generate-redirects.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const matter = require('gray-matter');
+
+const linksFilePath = path.join(__dirname, '../links/links.md');
+const redirectsFilePath = path.join(__dirname, '../_redirects');
+
+const fileContent = fs.readFileSync(linksFilePath, 'utf8');
+const { data } = matter(fileContent);
+
+const redirects = Object.entries(data).map(([key, url]) => `/${key} ${url} 302`).join('\n');
+
+fs.writeFileSync(redirectsFilePath, redirects);
+
+console.log('Redirects file generated successfully.');
